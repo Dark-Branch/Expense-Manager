87b400662373c8026aebdb266e6eb44955487761
Ke-vin-S <kevinxsanjula@gmail.com>
2025-02-06 19:24:34 +0530

    init

---
+++ src/test/java/com/bodimTikka/bodimTikka/BodimTikkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bodimTikka.bodimTikka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodimTikkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

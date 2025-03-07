091d9e6139eec33d3e91785b762359caffd30e05
kevin <kevinxsanjula@gmail.com>
2025-03-07 20:08:29 +0530

    change schema to use UUID for pk

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,23 +1,24 @@
 CREATE TABLE "user" (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE,      -- nullable for unregistered users
     password VARCHAR(255)           -- nullable
 );
 
 CREATE TABLE room (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE user_in_room (
-    id SERIAL PRIMARY KEY,
-    user_id INT,                         -- Nullable for unregistered users
-    room_id INT NOT NULL,
+    id UUID PRIMARY KEY,
+    user_id UUID,                         -- Nullable for unregistered users
+    room_id UUID NOT NULL,
     name VARCHAR(255) NOT NULL,          -- Display name for the room
     is_still_a_member BOOLEAN DEFAULT TRUE,
     is_admin BOOLEAN DEFAULT FALSE,
     is_registered BOOLEAN DEFAULT FALSE,
+    -- FIXME: is this needed
     FOREIGN KEY (user_id) REFERENCES "user"(id) ON DELETE CASCADE,
     FOREIGN KEY (room_id) REFERENCES room(id) ON DELETE CASCADE,
     UNIQUE (user_id, room_id)       -- Ensures a user can only be added once to a room
@@ -25,7 +26,7 @@ CREATE TABLE user_in_room (
 
 CREATE TABLE payment (
     payment_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    room_id INT NOT NULL,
+    room_id UUID NOT NULL,
     amount DECIMAL(10, 2) NOT NULL CHECK (amount > 0),
     description TEXT,
     is_repayment BOOLEAN DEFAULT FALSE,
@@ -36,10 +37,10 @@ CREATE TABLE payment (
 CREATE INDEX idx_payment_timestamp ON payment(payment_timestamp DESC);
 
 CREATE TABLE payment_record (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY,
     -- from_id < to_id
-    from_user_id INT NOT NULL,
-    to_user_id INT NOT NULL,
+    from_user_id UUID NOT NULL,
+    to_user_id UUID NOT NULL,
     amount DECIMAL(10, 2) NOT NULL CHECK (amount > 0),
     payment_id UUID NOT NULL,
     is_credit BOOLEAN NOT NULL,  -- Replaces 'direction' for better clarity

b096b85aa531ebc4df74f080eea6c375ec41b9fd
Kevin Sanjula <kevinxsanjula@gmail.com>
2025-08-26 13:54:33 +0530

    fix

--- src/main/java/com/bodimtikka/repository/TransactionRepository.java
+++ src/main/java/com/bodimtikka/repository/TransactionRepository.java
@@ -1,7 +1,6 @@
 package com.bodimtikka.repository;
 
 import com.bodimtikka.model.Transaction;
-import com.bodimtikka.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 

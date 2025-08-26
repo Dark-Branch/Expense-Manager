1bcacd6396effbb367b1c06cda6c248d50cf5cf6
Kevin Sanjula <kevinxsanjula@gmail.com>
2025-08-26 13:53:39 +0530

    Main application test

--- src/test/java/com/bodimtikka/BodimTikkaApplicationTests.java
+++ src/test/java/com/bodimtikka/BodimTikkaApplicationTests.java
@@ -2,8 +2,10 @@ package com.bodimtikka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = BodimTikkaApplication.class,webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class BodimTikkaApplicationTests {
 
 	@Test
